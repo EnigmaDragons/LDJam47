80196f82f9eedeb3a97389b79e79c9734411d7d5
MichaelElkan <melkan22@gmail.com>
2020-10-04 17:36:45 -0400

    added scene manager for dan

---
+++ src/LDJam47/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    #region Singleton
+
+    private static SceneLoader _instance;
+
+    public static SceneLoader SceneLoaderInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Game Manager is null!");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion Singleton
+
+    // Cached Components
+    Animator transitionAnimator;
+
+    private void Start()
+    {
+        transitionAnimator = GetComponentInChildren<Animator>();
+    }
+
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadScene(currentSceneIndex));
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadScene(currentSceneIndex + 1));
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadScene(0));
+    }
+
+    // If there is a save file, load scene that is not completed yet
+   
+
+    IEnumerator LoadScene(int buildIndex)
+    {
+        transitionAnimator.SetTrigger("loadLevel");
+        
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    IEnumerator LoadSceneByName(string sceneName)
+    {
+        transitionAnimator.SetTrigger("loadLevel");
+       
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
