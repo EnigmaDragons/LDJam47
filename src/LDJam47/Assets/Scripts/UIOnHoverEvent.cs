c0bc72e6cc78065aa5439447491f807414d22b19
Jaroslav Vozár <jarkovozar@gmail.com>
2020-10-05 13:09:56 +0200

    Animated game logo on mouse hover

---
+++ src/LDJam47/Assets/Scripts/UIOnHoverEvent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class UIOnHoverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] private float targetScaleModifier = 1.5f;
+	[SerializeField] private int animationsLengthInFrames = 30;
+
+	private Vector3 initialScale;
+	private bool    enlarging             = false;
+	private int     currentAnimationFrame = 0;
+
+	void Start()
+	{
+		initialScale = transform.localScale;
+	}
+
+	private void Update()
+	{
+		transform.localScale = initialScale * Mathf.SmoothStep(1, targetScaleModifier, currentAnimationFrame/(float)animationsLengthInFrames);
+
+		if (enlarging)
+		{
+			currentAnimationFrame++;
+		}
+		else
+		{
+			currentAnimationFrame--;
+		}
+
+		currentAnimationFrame = Mathf.Clamp(currentAnimationFrame, 0, animationsLengthInFrames);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		enlarging = true;
+		transform.SetAsLastSibling();
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		enlarging = false;
+	}
+}
