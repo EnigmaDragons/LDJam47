e2521f77bc9fc20d61b77a6efa13548225ff4b26
SilasReinagel <silas.reinagel@gmail.com>
2020-10-05 12:00:19 -0700

    Created basic Final Credits scene

---
+++ src/LDJam47/Assets/Scripts/Credits/CreditsPresenter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CreditsPresenter : MonoBehaviour
+{
+    [SerializeField] private FloatReference delayBeforeStart = new FloatReference(4f);
+    [SerializeField] private FloatReference delayBetween = new FloatReference(2.4f);
+    [SerializeField] private AllCredits allCredits;
+    [SerializeField] private UnityEvent onStart;
+    [SerializeField] private UnityEvent onFinished;
+    [SerializeField] private CreditPresenter creditPresenter;
+    [SerializeField] private FloatReference maxLifetimeOfCredit;
+
+    private void Start()
+    {
+        onStart.Invoke();
+        StartCoroutine(ShowNext());
+    }
+    
+    private IEnumerator ShowNext()
+    {
+        yield return new WaitForSeconds(delayBeforeStart);
+        
+        for (var i = 0; i < allCredits.Credits.Length; i++)
+        {
+            var presenter = Instantiate(creditPresenter, transform).Initialized(allCredits.Credits[i]);
+            Destroy(presenter.gameObject, maxLifetimeOfCredit);
+            yield return new WaitForSeconds(delayBetween);
+        }
+
+        onFinished.Invoke();
+    }
+}
