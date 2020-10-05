339def20e5ccfba9cbcf2d0aa80ef790e9a855de
SilasReinagel <silas.reinagel@gmail.com>
2020-10-05 14:03:57 -0700

    Staged basic bus scene.

---
+++ src/LDJam47/Assets/Scripts/OnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnStart : MonoBehaviour
+{
+    [SerializeField] private UnityEvent action;
+    [SerializeField] private FloatReference delay;
+
+    private void Start() => StartCoroutine(Go());
+
+    private IEnumerator Go()
+    {
+        yield return new WaitForSeconds(delay);
+        action.Invoke();
+    }
+}
